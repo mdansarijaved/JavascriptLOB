335dc55059d7b6223911979b97d7ce0c62809d38
Javed Ansari <javedans2003@gmail.com>
2023-02-13 23:08:03 +0530

    dom day 2

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,10 @@
 // alert("hello world ")
+document.querySelector("h1").style.color = "blue"
+document.querySelector("button").style.backgroundColor="red"; 
+document.querySelector("button").classList.add("invisible");
+
+// document.querySelector("button").classList.toggle("invisible"); 
+document.querySelector("h1").style.fontSize = "10rem"
+document.querySelector("h1").innerHTML = "<em>hello jee how are you</em> "
+
+document.querySelector("a").setAttribute("href","https://www.bing.com")
