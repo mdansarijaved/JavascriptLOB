bc84ddd75cc63a8c8c5c36406318111f3856567d
mdansarijaved <javedans2003@gmail.com>
2023-03-12 21:21:58 +0530

    map

--- OOP/map.js
+++ OOP/map.js
@@ -2,10 +2,15 @@
 
 
 let array1 = [1,2,3,4,5]; 
-let array2 = array1.map(n =>{
-    return n*2; 
-})
+// let array2 = array1.map(n =>{
+//     return n*2; 
+// })
+
+
+let array3 = array1.filter(x => x%2 == 0 || x%3==0)
 
 console.log(array1);
-console.log(array2);
+// console.log(array2);
+console.log(array3);
+
 
